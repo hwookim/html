26bd5ca5d65755148ed311d876e41057500456b5
hwookim <gusdn9754@gmail.com>
2020-11-23 15:54:15 +0900

    refactor: secret key

--- script/main.js
+++ script/main.js
@@ -1,6 +1,8 @@
+import FULLPAGE_KEY from "../secret";
+
 function init() {
     var myFullpage = new fullpage('#fullpage', {
-        licenseKey: 'C706A027-562E4D3C-85996AAA-AB219E10',
+        licenseKey: FULLPAGE_KEY,
         anchors:['1st', '2nd', '3rd', '4th', '6th', '7th'],
         navigation: true,
         navigationTooltips: ['Video', 'Home', 'History', 'etc'],
